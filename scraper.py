679378bdda8e1d569980854f23a5de0f1c279ca9
chieflaki <lazar.gugleta@student.turaz.at>
2019-07-10 01:30:40 +0200

    notebooksbilliger added to the project, works

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ URL = 'https://www.amazon.de/Kiebel-Gamer-PC-Goliath-184395-10x3-3GHz/dp/B003VN4
 
 headers = {"User-agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
 
-def check_price():
+def amazon_de():
 
     page = requests.get(URL, headers = headers)
 
@@ -24,7 +24,32 @@ def check_price():
     if (int(converted_price) < 2000):
         send_mail()
 
+link = 'https://www.notebooksbilliger.de/lg+34um88+p/eqsqid/7e3681c6-f6be-4f28-a254-671d99b3eadd'
 
+def notebooksbilliger():
+
+        page = requests.get(link, headers = headers)
+
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        #title
+        strip_title = str(soup.find("h1"))
+        strip_title1 = strip_title.replace('<h1 class="name squeezed">','')
+        strip_title2 = strip_title1.replace('</h1>','')
+        strip_title3 = strip_title2.replace('\n','')
+
+        #price
+        strip_price = soup.find("div", attrs={"id": "product_detail_price"})
+        strip_price1 = str(strip_price.find(attrs = {"class": "nbb-svg-base"}))
+        strip_price2 = strip_price1.replace("tspan dx=","")
+        strip_price3 = strip_price2[47:]
+        sep = ','
+        price = int(strip_price3.split(sep,1)[0])
+
+        print(price)
+        print(strip_title3.strip())
+        if (price < 400):
+                send_mail()  
 
 def send_mail():
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -36,7 +61,9 @@ def send_mail():
 
     subject = 'Price fell down!'
 
-    body = 'Check the amazon link: https://www.amazon.de/Kiebel-Gamer-PC-Goliath-184395-10x3-3GHz/dp/B003VN4BP8?pf_rd_p=012efe24-acb3-45ca-a485-03c9c1dfafdb&pd_rd_wg=u32Ls&pf_rd_r=3F00TKW241EPFY07J314&ref_=pd_gw_cr_wsim&pd_rd_w=YOyl5&pd_rd_r=e0787d9d-0a3c-460f-a050-1d6be6be2bf8&th=1'
+    body = 'Check the link:\
+    https://www.amazon.de/Kiebel-Gamer-PC-Goliath-184395-10x3-3GHz/dp/B003VN4BP8?pf_rd_p=012efe24-acb3-45ca-a485-03c9c1dfafdb&pd_rd_wg=\
+    u32Ls&pf_rd_r=3F00TKW241EPFY07J314&ref_=pd_gw_cr_wsim&pd_rd_w=YOyl5&pd_rd_r=e0787d9d-0a3c-460f-a050-1d6be6be2bf8&th=1'
 
     msg = f"Subject: {subject}\n\n{body}"
 
@@ -49,4 +76,5 @@ def send_mail():
 
     server.quit()
     
-check_price()
+notebooksbilliger()
+amazon_de()
