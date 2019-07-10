12b19e401a9ac09677783d8b569e2471c2d8b0b7
chieflaki <lazar.gugleta@student.turaz.at>
2019-07-10 10:10:50 +0200

    added edustore.at

--- scraper.py
+++ scraper.py
@@ -8,48 +8,72 @@ headers = {"User-agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KH
 
 def amazon_de():
 
-  page = requests.get(URL, headers = headers)
+    page = requests.get(URL, headers = headers)
 
-  soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-  title = soup.find(id = "productTitle").get_text()
-  price = soup.find(id="priceblock_ourprice").get_text()
-  sep = ','
-  con_price = price.split(sep, 1)[0]
-  converted_price = int(con_price.replace('.',''))
+    title = soup.find(id = "productTitle").get_text()
+    price = soup.find(id="priceblock_ourprice").get_text()
+    sep = ','
+    con_price = price.split(sep, 1)[0]
+    converted_price = int(con_price.replace('.',''))
 
-  print(converted_price)
-  print(title.strip())
+    print(title.strip())
+    print(converted_price)
 
-  if (int(converted_price) < 2000):
-    send_mail()
+    if (int(converted_price) < 2000):
+        send_mail()
+    else:
+        print("Price has not dropped!\n")
 
 link = 'https://www.notebooksbilliger.de/lg+34um88+p/eqsqid/7e3681c6-f6be-4f28-a254-671d99b3eadd'
 
 def notebooksbilliger():
 
-  page = requests.get(link, headers = headers)
+    page = requests.get(link, headers = headers)
 
-  soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, 'html.parser') 
 
-  #title
-  strip_title = str(soup.find("h1"))
-  strip_title1 = strip_title.replace('<h1 class="name squeezed">','')
-  strip_title2 = strip_title1.replace('</h1>','')
-  strip_title3 = strip_title2.replace('\n','')
+    #title
+    strip_title = str(soup.find("h1"))
+    strip_title1 = strip_title.replace('<h1 class="name squeezed">','')
+    strip_title2 = strip_title1.replace('</h1>','')
+    strip_title3 = strip_title2.replace('\n','')
 
-  #price
-  strip_price = soup.find("div", attrs={"id": "product_detail_price"})
-  strip_price1 = str(strip_price.find(attrs = {"class": "nbb-svg-base"}))
-  strip_price2 = strip_price1.replace("tspan dx=","")
-  strip_price3 = strip_price2[47:]
-  sep = ','
-  price = int(strip_price3.split(sep,1)[0])
+    #price
+    strip_price = soup.find("div", attrs={"id": "product_detail_price"})
+    strip_price1 = str(strip_price.find(attrs = {"class": "nbb-svg-base"}))
+    strip_price2 = strip_price1.replace("tspan dx=","")
+    strip_price3 = strip_price2[47:]
+    sep = ','
+    price = int(strip_price3.split(sep,1)[0])
 
-  print(price)
-  print(strip_title3.strip())
-  if (price < 300):
-    send_mail()  
+    print(strip_title3.strip())
+    print(price)
+    if (price < 300):
+        send_mail()
+    else:
+        print("Price has not dropped!\n")
+
+linked = 'https://www.edustore.at/lenovo-thinkvision-p44w-led-display-110-2-cm-43-4-zoll-ultrawide-full-hd-gebogen-matt-schwarz'
+
+def edustore():
+    page = requests.get(linked, headers = headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    #title
+    title = str(soup.find("h1").text)
+    strip_price = str(soup.find("span", attrs={"class": "price"}))
+    price = int(''.join(i for i in strip_price if i.isdigit()))
+
+    print(title)
+    print(price)
+
+    if (price < 1000):
+        send_mail()
+    else:
+        print("Price has not dropped!\n")
+
 
 def send_mail():
   server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -68,13 +92,14 @@ def send_mail():
   msg = f"Subject: {subject}\n\n{body}"
 
   server.sendmail(
-    'Price check',
-    'lazar.gugleta@gmail.com',
-    msg
+   'Price check',
+   'lazar.gugleta@gmail.com',
+   msg
   )
   print('Hey Email has been sent!')
 
   server.quit()
-    
+   
 notebooksbilliger()
 amazon_de()
+edustore()
